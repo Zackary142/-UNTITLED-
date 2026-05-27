32972ac738d04db3581dd6b5fefbbffbc47173a0
Zack_142 <50596061+Zackary142@users.noreply.github.com>
2026-05-27 19:01:06 +0000

    Refined changes to landing page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -6,6 +6,10 @@ function App() {
       <main className="landing-page">
         <h1 className="brand-title">InvaQuote</h1>
         <p className="brand-subtitle">Quote, Invoice, That's it.</p>
+        <div className="brand-footer">
+          <span className="footer-left">Coming this July...</span>
+          <span className="footer-right">Contact us:</span>
+        </div>
       </main>
     </>
   )
